d05fcfdba103812b99e96ea492540fe6f3d864b3
Diana <dianiv_26@hotmail.com>
2020-03-31 16:07:39 -0500

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+    const datos = new Uint8Array(Buffer.from(data));
+    fs.writeFile('por-hacer/db/data.json', datos, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+
+
+        // console.log(`El archivo tabla-${base}.txt ha sido creado`);
+    });
+}
+
+
+const cargarDB = () => {
+
+    try {
+
+        listadoPorHacer = require('../por-hacer/db/data.json');
+
+
+    } catch (error) {
+
+        listadoPorHacer = [];
+    }
+    // console.log(listadoPorHacer);
+
+}
+
+
+
+const getListado = () => {
+
+    cargarDB();
+
+    return listadoPorHacer;
+}
+
+
+
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false,
+
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    //va a regresar -1 si es que no lo encuentra
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+
+    } else {
+
+        return false;
+    }
+
+
+}
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+    let nuevoListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+}
+
+
+module.exports = {
+
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
